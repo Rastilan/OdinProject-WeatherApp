248e94049c2c14fe14f1a686594c99708c43b4aa
Rastilan <christoperbhall@gmail.com>
2022-06-15 02:31:13 -0400

    Now checks for invalid locations

--- src/index.js
+++ src/index.js
@@ -7,13 +7,21 @@ window.SubmitClick = () => {
   Submit(submittedCity)
 }
 async function Submit(city) {
+
   const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${city}&appid=62210f355f86e20464dff962fe8f357b`, {mode: 'cors'});
   const weatherData = await response.json();
-  let outputDiv = document.getElementById('output');
-  document.getElementById('city').innerHTML = "City: " + weatherData.name;
-  document.getElementById('temp').innerHTML = "Temp: " + weatherData.main.temp + "F";
-  document.getElementById('clouds').innerHTML = "Clouds: " + weatherData.weather[0].description
-  document.getElementById('feels-like').innerHTML = "Feels Like: " + weatherData.main.feels_like + "F";
+  if(weatherData.name === undefined){
+    alert("could not locate city or state by that name");
+  }
+  else {
+    // let outputDiv = document.getElementById('output');
+    document.getElementById('city').innerHTML = "City: " + weatherData.name;
+    document.getElementById('temp').innerHTML = "Temp: " + weatherData.main.temp + "F";
+    document.getElementById('clouds').innerHTML = "Clouds: " + weatherData.weather[0].description
+    document.getElementById('feels-like').innerHTML = "Feels Like: " + weatherData.main.feels_like + "F";
+  }
+
+
 
 }
 window.Submit = Submit;
